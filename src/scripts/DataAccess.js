dd66fccc8e8d3769cda2ca9d447266a8dce173b1
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-08-18 15:57:26 -0500

    added fetch functions in DataAccess.js

--- src/scripts/DataAccess.js
+++ src/scripts/DataAccess.js
@@ -6,3 +6,32 @@ const applicationState = {
 
 const API = "http://localhost:8088"
 
+export const fetchPenPals = () => {
+    return fetch(`${API}/penPals`)
+    .then (res => res.json())
+    .then (
+        (fetchedPenPals) => {
+            applicationState.penPals = fetchedPenPals
+        }
+    )
+}
+
+export const fetchTopics = () => {
+    return fetch(`${API}/topics`)
+    .then (res => res.json())
+    .then (
+        (fetchedTopics) => {
+            applicationState.topics = fetchedTopics
+        }
+    )
+}
+
+export const fetchLetters = () => {
+    return fetch(`${API}/letters`)
+    .then (res => res.json())
+    .then (
+        (fetchedLetters) => {
+            applicationState.letters = fetchedLetters
+        }
+    )
+}
