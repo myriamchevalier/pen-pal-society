0c5e172a22cdbc1cb8144d695983e3d114f91e09
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-08-26 14:27:55 -0500

    added module LetterForm.js, created a function exporting the whole form including Authors, Topics and Recipients

--- src/scripts/PenPalSociety.js
+++ src/scripts/PenPalSociety.js
@@ -1,19 +1,12 @@
+import { LetterForm } from "./LetterForm.js"
+
 //This module will contain the HTML frame for the whole page
-
-import { Authors } from "./Authors.js"
-import { Recipients } from "./Recipients.js"
-import { Topics } from "./Topics.js"
-
-
 export const PenPalSociety = () => {
     return `
         <h1>Pen Pal Society</h1>
         
         <section id="letterForm">
-        ${Authors()}
-        ${Recipients()}
-        ${Topics()}
-            Insert LetterForm module here
+        ${LetterForm()}
         </section>
 
         <section id="letter">
